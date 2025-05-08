5a1d64b7fe61d1f0fc98382a5260d9e3f922f3e8
aspire của sduy <sonduy436@gmail.com>
2025-05-08 16:58:37 +0700

    n

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,5 +1,15 @@
 package src;
 
+import javax.swing.SwingUtilities;
+
 public class Launcher {
-    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainWindow main = new MainWindow();
+                main.show();
+            }
+        });
+    }
 }
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,5 +1,19 @@
 package src;
 
+import javax.swing.JFrame;
+
 public class MainWindow {
-    
+    private JFrame window;
+
+    public MainWindow() {
+        window = new JFrame();
+        window.setTitle("Game Ban Ga");
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        window.setSize(800, 500);
+        window.setLocationRelativeTo(null);
+    }
+
+    public void show() {
+        window.setVisible(true);
+    }
 }
