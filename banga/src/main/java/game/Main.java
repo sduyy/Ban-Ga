9e282ebc3cf652057cde278ae57210ddf1d29c99
mac của sduy <sonduy436@gmail.com>
2025-05-09 16:00:59 +0700

    n

--- banga/src/main/java/game/Main.java
+++ banga/src/main/java/game/Main.java
@@ -15,7 +15,6 @@ public class Main extends Application{
         primaryStage.setScene(scene);
         primaryStage.setTitle("Game Ban Ga");
         primaryStage.show();
-
     }
     public static void main(String[] args) {
         launch(args);
